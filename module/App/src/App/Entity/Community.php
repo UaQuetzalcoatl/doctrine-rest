8144ea5446479ff37332d2be0fbd697b17d5aa9a
Alex <ua.quetzalcoatl@gmail.com>
2015-07-05 14:45:05 +0300

    community enhancement

--- module/App/src/App/Entity/Community.php
+++ module/App/src/App/Entity/Community.php
@@ -61,4 +61,155 @@ class Community extends AbstractEntity
         $this->users = new ArrayCollection;
         $this->userGroups = new ArrayCollection;
     }
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getMaxUsers()
+    {
+        return $this->maxUsers;
+    }
+
+    public function getMaxUsersGroups()
+    {
+        return $this->maxUsersGroups;
+    }
+
+    public function getUsers()
+    {
+        return $this->users;
+    }
+
+    public function getUserGroups()
+    {
+        return $this->userGroups;
+    }
+
+    public function getIsBlocked()
+    {
+        return $this->isBlocked;
+    }
+
+    /**
+     * Set communtiy name
+     * 
+     * @param string $name
+     * @return \App\Entity\Community
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        
+        return $this;
+    }
+
+    /**
+     * Set max number of users. Null = unlimited.
+     * 
+     * @param int|null $maxUsers
+     * @return \App\Entity\Community
+     */
+    public function setMaxUsers($maxUsers)
+    {
+        $this->maxUsers = $maxUsers;
+        
+        return $this;
+    }
+
+    /**
+     * Set max number of user groups. Null = unlimited.
+     * 
+     * @param int|null $maxUsersGroups
+     * @return \App\Entity\Community
+     */
+    public function setMaxUsersGroups($maxUsersGroups)
+    {
+        $this->maxUsersGroups = $maxUsersGroups;
+        
+        return $this;
+    }
+
+    /**
+     * Add user to community
+     * 
+     * @param \App\Entity\User $user
+     * @return \App\Entity\Community
+     * @throws \RuntimeException
+     */
+    public function addUser(User $user)
+    {
+        if (null !== $this->maxUsers && $this->maxUsers <= $this->users->count()) {
+            throw new \RuntimeException('Max number of community users reached');
+        }
+        
+        $this->users->add($user);
+        $user->setCommunity($this);
+        
+        return $this;
+    }
+    
+    /**
+     * Remove user from community
+     * 
+     * @param \App\Entity\User $user
+     * @return \App\Entity\Community
+     */
+    public function removeUser(User $user)
+    {
+        $this->users->removeElement($user);
+        
+        return $this;
+    }
+    
+    /**
+     * Add user group to community
+     * 
+     * @param \App\Entity\UserGroup $userGroup
+     * @return \App\Entity\Community
+     * @throws \RuntimeException
+     */
+    public function addUserGroup(UserGroup $userGroup)
+    {
+        if (null !== $this->maxUsersGroups && $this->maxUsersGroups <= $this->userGroups->count()) {
+            throw new \RuntimeException('Max number of user groups reached');
+        }
+        
+        $this->userGroups->add($userGroup);
+        $userGroup->setCommunity($this);
+        
+        return $this;
+    }
+    
+    /**
+     * Remove user group from community
+     * 
+     * @param \App\Entity\UserGroup $userGroup
+     * @return \App\Entity\Community
+     * @throws \InvalidArgumentException
+     */
+    public function removeUserGroup(UserGroup $userGroup)
+    {
+        if ($userGroup->getIsSystem()) {
+            throw new \InvalidArgumentException('User group is system and can\'t be deleted');
+        }
+        
+        $this->userGroups->removeElement($userGroup);
+        
+        return $this;
+    }
+
+    /**
+     * Block/Unblock community
+     * 
+     * @param bool $isBlocked
+     * @return \App\Entity\Community
+     */
+    public function setIsBlocked($isBlocked)
+    {
+        $this->isBlocked = (bool) $isBlocked;
+        
+        return $this;
+    }
 }
